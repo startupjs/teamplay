9f3e9494bc297989116202cfcca417f2d2ed0e6c
Pavel Zhukov <cray0000@gmail.com>
2024-06-06 06:20:50 +0300

    feat(teamplay): support passing array to sub() for multiple parallel subscriptions

--- packages/teamplay/orm/sub.js
+++ packages/teamplay/orm/sub.js
@@ -3,9 +3,16 @@ import { docSubscriptions } from './Doc.js'
 import { querySubscriptions, getQuerySignal } from './Query.js'
 
 export default function sub ($signal, params) {
+  if (Array.isArray($signal)) {
+    const res = $signal.map(args => Array.isArray(args) ? sub(...args) : sub(args))
+    if (res.some($s => $s.then)) return Promise.all(res)
+    return res
+  }
   if (isPublicDocumentSignal($signal)) {
+    if (arguments.length > 1) throw Error(ERRORS.subDocArguments(...arguments))
     return doc$($signal)
   } else if (isPublicCollectionSignal($signal)) {
+    if (arguments.length !== 2) throw Error(ERRORS.subQueryArguments(...arguments))
     return query$($signal, params)
   } else if (typeof $signal === 'function' && !($signal instanceof Signal)) {
     return api$($signal, params)
@@ -21,6 +28,7 @@ function doc$ ($doc) {
 }
 
 function query$ ($collection, params) {
+  if (typeof params !== 'object') throw Error(ERRORS.queryParamsObject($collection, params))
   const $query = getQuerySignal($collection[SEGMENTS], params)
   const promise = querySubscriptions.subscribe($query)
   if (!promise) return $query
@@ -29,4 +37,28 @@ function query$ ($collection, params) {
 
 function api$ (fn, args) {
   throw Error('sub() for async functions is not implemented yet')
+}
+
+const ERRORS = {
+  subDocArguments: ($signal, ...args) => `
+    sub($doc) accepts only 1 argument - the document signal to subscribe to
+    Doc: ${$signal[SEGMENTS]}
+    Got args: ${[$signal, ...args]}
+  `,
+  subQueryArguments: ($signal, params, ...args) => `
+    sub($collection, params) accepts 2 arguments - the collection signal and an object with query params.
+    If you want to subscribe to all documents in a collection, pass an empty object: sub($collection, {}).
+    Collection: ${$signal[SEGMENTS]}
+    Params: ${params}
+    Got args: ${[$signal, params, ...args]}
+  `,
+  queryParamsObject: ($collection, params) => `
+    sub($collection, params):
+      Params must be an object.
+      If you want to subscribe to all documents in a collection, pass an empty object: sub($collection, {}).
+
+      Got:
+        $collection: ${$collection[SEGMENTS]}
+        params: ${params}
+  `
 }
