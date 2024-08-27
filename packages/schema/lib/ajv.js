66f92f9e289f7fc9a3c50cb9ff25cbc17036fb3a
Pavel Turchik <pvturchik@gmail.com>
2024-08-27 12:37:17 +0300

    feat(@teamplay/schema): add 'ajv-errors' for custom error message

--- packages/schema/lib/ajv.js
+++ packages/schema/lib/ajv.js
@@ -1,5 +1,11 @@
 import Ajv from 'ajv'
+import ajvErrors from 'ajv-errors'
 
-const ajv = new Ajv({ strict: false })
+const ajv = new Ajv({
+  allErrors: true,
+  strict: false
+})
+
+ajvErrors(ajv)
 
 export default ajv
