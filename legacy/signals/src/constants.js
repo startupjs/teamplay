5368d189fb095b701da2ef1ba5d420812af6e578
Pavel Zhukov <cray0000@gmail.com>
2025-05-21 16:45:54 +0300

    chore: remove legacy signals implementation (which was used as a reference)

--- legacy/signals/src/constants.js
+++
@@ -1,5 +0,0 @@
-const LOCAL_COLLECTION_REGEX = /^(_|$)/
-
-export const isLocalCollection = collectionName => LOCAL_COLLECTION_REGEX.test(collectionName)
-export const isRemoteDocSegments = segments => segments.length === 2 && !isLocalCollection(segments[0])
-export const isRemoteDocDataSegments = segments => segments.length >= 2 && !isLocalCollection(segments[0])
